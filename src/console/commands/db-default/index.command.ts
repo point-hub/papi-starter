2fec6ec5f9e7564604ac4b8759ec811ccf55756f
Martien Dermawan Tanama <martiendt@gmail.com>
2025-01-05 14:16:33 +0700

    feat: add seeding database command for default entries and demo purpose

---
+++ src/console/commands/db-default/index.command.ts
@@ -0,0 +1,44 @@
+import { BaseConsoleCommand, BaseDatabaseConnection, BaseMongoDBConnection } from '@point-hub/papi'
+
+import mongoDBConfig from '@/config/mongodb'
+
+export default class DbSeedCommand extends BaseConsoleCommand {
+  dbConnection = new BaseDatabaseConnection(new BaseMongoDBConnection(mongoDBConfig.url, mongoDBConfig.name))
+
+  constructor() {
+    super({
+      name: 'db:default',
+      description: 'Populate database with default entries',
+      summary: 'Populate database with default entries',
+      arguments: [],
+      options: [],
+    })
+  }
+  async handle(): Promise<void> {
+    let session
+    try {
+      await this.dbConnection.open()
+      session = this.dbConnection.startSession()
+      session.startTransaction()
+      /**
+       * Usage
+       * ex: await this.seeds(['directory'], { session })
+       */
+    } catch (error) {
+      console.error(error)
+      await session?.abortTransaction()
+    } finally {
+      await session?.commitTransaction()
+      await session?.endSession()
+      this.dbConnection.close()
+    }
+  }
+  private async seeds(directories: string[], options: unknown): Promise<void> {
+    for (const directory of directories) {
+      // import seed function
+      const { seed } = await import(`@/modules/${directory}/default.seed`)
+      // seed database
+      await seed(this.dbConnection, options)
+    }
+  }
+}
---
+++ src/console/commands/db-demo/index.spec.ts
@@ -0,0 +1,11 @@
+import { expect, it, spyOn } from 'bun:test'
+
+import SeedCommand from './index.command'
+
+it('test command', async () => {
+  const seedCommand = new SeedCommand()
+  const spy = spyOn(seedCommand, 'handle')
+  await seedCommand.handle()
+
+  expect(spy).toHaveBeenCalled()
+})
