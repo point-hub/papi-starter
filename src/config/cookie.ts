1ff0f5918dbef55e457e180eaeb31778b5ffe268
Martien Dermawan Tanama <martiendt@gmail.com>
2025-01-08 18:45:28 +0700

    chore: add typescript interface

--- src/config/cookie.ts
+++ src/config/cookie.ts
@@ -1,5 +1,11 @@
+export interface ICookieConfig {
+  secret: string
+}
+
 export const secret = process.env['COOKIE_SECRET'] ?? ''
 
-export default {
+const cookieConfig: ICookieConfig = {
   secret,
 }
+
+export default cookieConfig
--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,14 @@
+export interface ICorsConfig {
+  origin: string
+  credentials: boolean
+}
+
 export const origin = process.env['CORS_ORIGIN'] ?? ''
 export const credentials: boolean = process.env['CORS_CREDENTIALS'] === 'true' ? true : false
 
-export default {
+const corsConfig: ICorsConfig = {
   origin,
   credentials,
 }
+
+export default corsConfig
